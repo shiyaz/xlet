5e046bffa1d971ca3c640e0c95d2e8c702b8cfe8
Shiyaz Rasheed <shiyaz@gmail.com>
2016-02-06 22:42:39 +0000

    Fix project.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject xlet "0.1.0-SNAPSHOT"
-  :description "Clojure let binding with Scala-like pattern extraction."
+(defproject atollier.xlet/xlet "0.1.0-SNAPSHOT"
+  :description "Clojure let binding using Scala-like pattern extraction."
   :url "http://github.com/shiyaz/xlet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
